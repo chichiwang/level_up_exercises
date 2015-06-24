a36e0da280dab3abb6b0e281c20cb399390dbd77
Chi-chi Wang <chichi@wangchiyi.com>
2015-06-24 13:25:58 -0500

    [dino_catalog] Make error messages constants in Commands

--- dino_catalog/commands.rb
+++ dino_catalog/commands.rb
@@ -1,16 +1,21 @@
 module Commands
   COMMANDS = %w(help list export exit manual)
+  ERROR = {
+    cmd: "> Unrecognized command: ",
+    flag: "> Invalid flag: ",
+    params: "> Unrecognized parameters: ",
+  }
 
   def self.err_cmd(cmd)
-    "> Unrecognized command: #{cmd}\n\n"
+    ERROR[:cmd] + "#{cmd}\n\n"
   end
 
   def self.err_flag(param)
-    "> Invalid flag: #{param.first}\n\n"
+    ERROR[:flag] + "#{param.first}\n\n"
   end
 
   def self.err_params(params)
-    "> Unrecognized parameters: #{params}\n\n"
+    ERROR[:params] + "#{params}\n\n"
   end
 
   def self.valid_cmd?(cmd)
