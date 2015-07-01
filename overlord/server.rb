e27abc5e4ddd352ac5f5132ed19f39182ba57efb
Chi-chi Wang <chichi@wangchiyi.com>
2015-07-01 12:10:14 -0500

    [overlord] Add client app as a submodule

--- overlord/server.rb
+++ overlord/server.rb
@@ -1,7 +1,28 @@
 require 'sinatra/base'
+require 'sinatra-websocket'
 
 class Server < Sinatra::Base
+  set :public_folder, proc { File.join(root, "client", "public") }
+  set :sockets, []
+
   get '/' do
-    erb :index
+    if request.websocket?
+      request.websocket do |ws|
+        ws.onopen do
+          ws.send("Hello World!")
+          settings.sockets << ws
+          puts "socket opened"
+        end
+        ws.onmessage do |msg|
+          # EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+        end
+        ws.onclose do
+          warn("websocket closed")
+          settings.sockets.delete(ws)
+        end
+      end
+    else
+      send_file File.join(settings.public_folder, 'index.html')
+    end
   end
 end
